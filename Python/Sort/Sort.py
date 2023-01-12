10a8f8431aa9cbdead3178b7959aa92b4fe758ca
stelliclife <new11dreamer@gmail.com>
2023-01-13 07:29:58 +0900

    Added two versions of Insertion sort

---
+++ Python/Sort/Sort.py
@@ -0,0 +1,60 @@
+class SortingAlgorithms:
+    def __init__(self, array):
+        self._array = array
+
+    @property
+    def array(self):
+        return self._array
+
+    def insertion_sort_double_loop(self):
+        i = 1
+        length = len(self._array)
+
+        while i < length:
+            j, k = i - 1, length
+            target = self._array[i]
+            while j >= 0:
+                if target < self._array[j]:
+                    k = j
+                j -= 1
+            if k != length:
+                temp = self._array[k:i]
+                self._array[k] = target
+                self._array[k+1:i+1] = temp
+            i += 1
+
+    def insertion_sort_one_loop(self):
+        i, j= 1, 0
+        length = len(self._array)
+        k = length
+
+        while True:
+            if i == length:
+                break
+            target = self._array[i]
+            if j < 0:
+                if k != length:
+                    temp = self._array[k:i]
+                    self._array[k] = target
+                    self._array[k+1:i+1] = temp
+                i += 1
+                j = i - 1
+            else:
+                if target < self._array[j]:
+                    k = j
+                j -= 1
+
+    def selection_sort(self):
+        return
+
+    def bubble_sort(self):
+        return
+
+    def merge_sort(self):
+        return
+
+    def quick_sort(self):
+        return
+
+    def heap_sort(self):
+        return
